2e0d82142636c44e2bba108f93ced7930ca76d1c
Mohd Ramzan Shareef <107868540+ramzanshareef@users.noreply.github.com>
2024-07-18 12:34:42 +0000

    CHORE: Update .gitignore and page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,15 @@ import Link from "next/link";
 export default function Home() {
     return (
         <>
-            <div
-                className="flex flex-col items-center justify-center min-h-screen py-2"
-            >
-                Welcome to the NextJS Auth Template <Link href="/login"
-                    className="p-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700"
-                >Login</Link> or <Link href="/signup"
-                    className="p-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700"
-                >Signup</Link> to get started
+            <div className="flex flex-col items-center justify-center min-h-screen py-2">
+                Welcome to the Email Scheduler <Link href="/login"
+                    className="p-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700" >Login</Link> or <Link href="/signup" className="p-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700">Signup</Link> to get started
             </div>
         </>
     );
 }
+
+export const metadata = {
+    title: "Email Scheduler",
+    description: "Welcome to the Email Scheduler - Login or Signup to get started",
+};
